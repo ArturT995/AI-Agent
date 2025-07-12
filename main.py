29841225884e6f6af951b8975d298e4303548269
ArturT995 <arturtre95@gmail.com>
2025-07-12 16:54:45 +0000

    K: Added LLM call loop

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from google.genai import types
 from dotenv import load_dotenv
 
 from prompts import system_prompt
-from call_function import call_function, available_functions
+from call_function import *
 
 
 def main():
@@ -36,35 +36,48 @@ def main():
 
 
 def generate_content(client, messages, verbose):
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions], system_instruction=system_prompt
-        ),
-    )
-    if verbose:
-        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    for i in range (0, 20):
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents = messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions], system_instruction=system_prompt
+            ),
+        )
 
-    if not response.function_calls:
-        return response.text
-
-    function_responses = []
-    for function_call_part in response.function_calls:
-        function_call_result = call_function(function_call_part, verbose)
-        if (
-            not function_call_result.parts
-            or not function_call_result.parts[0].function_response
-        ):
-            raise Exception("empty function call result")
+        
         if verbose:
-            print(f"-> {function_call_result.parts[0].function_response.response}")
-        function_responses.append(function_call_result.parts[0])
+            print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+            print("Response tokens:", response.usage_metadata.candidates_token_count)
+        
+        for candidate in response.candidates:
+            messages.append(candidate.content)
+        
+        if not response.function_calls:
+            print(response.text)
+            return response.text
+            break
+        
+        function_responses = []
+        for function_call_part in response.function_calls:
+            function_call_result = call_function(function_call_part, verbose)
+            if (
+                not function_call_result.parts
+                or not function_call_result.parts[0].function_response
+            ):
+                raise Exception("empty function call result")
+            if verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+            function_responses.append(function_call_result.parts[0])
 
-    if not function_responses:
-        raise Exception("no function responses generated, exiting.")
+        if not function_responses:
+            raise Exception("no function responses generated, exiting.")
 
+        tool = types.Content(
+            role="tool",
+            parts=function_responses
+        )
+        messages.append(tool)
 
 if __name__ == "__main__":
     main()
